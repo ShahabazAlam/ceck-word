8bfb84b3741ca0db9e14de0f3d95610bc1763aad
@SalamAlam189 <Salam@Ramognee.in>
2021-04-18 15:27:59 +0530

    heroku commit

--- main/views.py
+++ main/views.py
@@ -8,10 +8,13 @@ import spacy
 class Main(APIView):
 
     def post(self,request):
-        nlp = spacy.load('en_core_web_sm')
-        about_doc = nlp(request.data['paragraph'])
-        data = []
-        for token in about_doc:
-            print (type(str(token)))
-            data.append({'word':str(token), 'pos':token.pos_ ,'explain':spacy.explain(token.tag_)})
-        return Response(data)
+        try:
+            nlp = spacy.load('en_core_web_sm')
+            about_doc = nlp(request.data['paragraph'])
+            data = []
+            for token in about_doc:
+                print (type(str(token)))
+                data.append({'word':str(token), 'pos':token.pos_ ,'explain':spacy.explain(token.tag_)})
+            return Response(data)
+        except:
+            print("An exception occurred")
